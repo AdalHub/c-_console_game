8582c610a3be05bd438f2cc19da75830252e6210
adalhub <adalteran@gmail.com>
2026-06-01 14:35:54 -0500

    first commit

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Player
+    {
+        public string name = "playa";
+        int health = 100;
+        public int attackDamage = 10;
+        public void attack()
+        {
+            Console.WriteLine($"You just attacked {attackDamage}");
+        }
+
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleApp1;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Player myPlayer = new Player();
+        Console.WriteLine("Enter name");
+        myPlayer.name = Console.ReadLine();
+        
+        Console.WriteLine($"Welcome to console game, {myPlayer.name}");
+        
+        Console.WriteLine("For attack enter: A");
+        string userInput = Console.ReadLine();
+        if(userInput == "A")
+        {
+            Console.WriteLine($"You just did {myPlayer.attackDamage} damage");
+        }
+    }
+}
