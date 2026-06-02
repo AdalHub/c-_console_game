31b470ce88a32941c726d1798980259d5e691d47
adalhub <adalteran@gmail.com>
2026-06-02 16:16:14 -0500

    feat: added sword and gun classes to items

--- ConsoleApp1/Items.cs
+++ ConsoleApp1/Items.cs
@@ -13,13 +13,14 @@ namespace ConsoleApp1
         public int purchaseCost;
         public int sellCost;
         public bool usable;
+        public float accuracy;
 
         abstract public float use();
     }
 
     class Sword: Items {
         
-        public Sword(string name, float durability, float damage, int purchaseCost, int sellCost)
+        public Sword(string name, float durability, float damage, int purchaseCost, int sellCost, float accuracy)
         {
             condition = 100.0F;
             usable = true;
@@ -29,7 +30,9 @@ namespace ConsoleApp1
             this.damage = damage;
             this.purchaseCost = purchaseCost;
             this.sellCost = sellCost;
-            
+            this.accuracy= accuracy;
+
+
         }
 
         public override float use()
@@ -38,8 +41,60 @@ namespace ConsoleApp1
                 Console.WriteLine($"{Name} is broken!");
                 return 0f; 
             }
+
             condition -= 1 * durability;
-            return damage;
+
+            Random rand = new Random();
+
+            // for swords they the damage delt depends on the effectiveness of the hit
+            float totalDamage = damage * (rand.Next(1, 100) * .001f);
+            Console.WriteLine($"You took a jab! \n {totalDamage} was delt.");
+            return totalDamage;
+        }
+
+    }
+
+    class Gun : Items
+    {
+
+        public Gun(string name, float durability, float damage, int purchaseCost, int sellCost, float accuracy)
+        {
+            condition = 100.0F;
+            usable = true;
+
+            Name = Name;
+            this.durability = durability;
+            this.damage = damage;
+            this.purchaseCost = purchaseCost;
+            this.sellCost = sellCost;
+            this.accuracy= accuracy;
+
+        }
+
+        public override float use()
+        {
+            if (!usable)
+            {
+                Console.WriteLine($"{Name} is broken!");
+                return 0f;
+            }
+            condition -= 1 * durability;
+
+            Random rand = new Random();
+            
+            //For guns if the random number is within the accuracy range it will be successful
+            if(rand.Next(1, 100) <= accuracy)
+            {
+                Console.WriteLine("Your shot landed!");
+                return damage;
+            }
+            else
+            {
+                Console.WriteLine("Your shot missed! \n 0 damage delt.");
+                return 0f;
+            }
+            
+            
         }
 
     }
